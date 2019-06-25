7a6cd88d3b1cb8ef637ab0a5c8129283e4fcd719
pangyajing <pangyajing@ainirobot.com>
2019-06-25 16:01:21 +0800

    fix system

--- bin/react-admin-app.js
+++ bin/react-admin-app.js
@@ -9,7 +9,9 @@
 const meow = require('meow')
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 const { execSync, spawnSync } = require('child_process');
+const log = console.log;
 
 const cli = meow(`
   Usage
@@ -27,7 +29,7 @@ try {
   fs.mkdirSync(`./${projectName}`);
 }
 
-const cwdOriginal = path.join(path.resolve(path.resolve(__dirname, '..')), 'template/')
+const cwdOriginal = path.join(path.resolve(path.resolve(__dirname, '..')), 'template')
 const cwd = path.join(process.cwd(), projectName)
 
 let data = fs.readFileSync(path.join(cwdOriginal ,'package.json'),"utf-8");
@@ -41,8 +43,47 @@ fs.writeFileSync(path.join(cwdOriginal ,'package.json'), JSON.stringify(data, nu
 
 copyDir(cwdOriginal, cwd)
 
-function copyDir(src, dist) {
-  spawnSync('cp', ['-r', src, dist]);	
+// function copyDir(src, dist) {
+//   spawnSync('cp', ['-r', src, dist]);	
+// }
+function copyDir(src, dist, callback) {
+  fs.access(dist, function(err){
+    if(err){
+      // 目录不存在时创建目录
+      fs.mkdirSync(dist);
+    }
+    _copy(null, src, dist);
+  });
+
+  function _copy(err, src, dist) {
+    if(err){
+      callback(err);
+    } else {
+      fs.readdir(src, function(err, paths) {
+        if(err){
+          callback(err)
+        } else {
+          paths.forEach(function(path) {
+            var _src = src + '/' +path;
+            var _dist = dist + '/' +path;
+            fs.stat(_src, function(err, stat) {
+              if(err){
+                callback(err);
+              } else {
+                // 判断是文件还是目录
+                if(stat.isFile()) {
+                  fs.writeFileSync(_dist, fs.readFileSync(_src));
+                } else if(stat.isDirectory()) {
+                  // 当是目录是，递归复制
+                  copyDir(_src, _dist, callback)
+                }
+              }
+            })
+          })
+        }
+      })
+    }
+  }
 }
 
 function myInExecSync(cmd) {
@@ -69,11 +110,16 @@ try {
   console.log('Installing react, react-dom, and ...')
 
   myOutExecSync('npm install');
+  myOutExecSync('npm list --depth=0');
 
-  console.log('Happy hacking!')
-  console.log(`cd ${projectName} && yarn start`)
+  log('')
+  log('We suggest that you begin by typing:')
+  log('')
+  log(chalk.green('cd ') + projectName + chalk.green(' && yarn start'));
+  log('')
+  log('Happy hacking!')
 } catch (e) {
-  console.log(e)
+  log(e)
 }
 
 
